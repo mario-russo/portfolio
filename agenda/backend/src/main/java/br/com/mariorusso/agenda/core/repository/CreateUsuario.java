7596b14f87ca7c64d7cc8344e3056203255b0bbb
Mário Russo <mario.russo93@hotmail.com>
2022-11-11 19:18:08 -0300

    feat (repositório) : salvar usuário

---
+++ agenda/backend/src/main/java/br/com/mariorusso/agenda/core/repository/CreateUsuario.java
@@ -0,0 +1,7 @@
+package br.com.mariorusso.agenda.core.repository;
+
+import br.com.mariorusso.agenda.core.model.Usuario;
+
+public interface CreateUsuario {
+    public Usuario execute (Usuario usuario);
+}
