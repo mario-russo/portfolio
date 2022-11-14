8be8501e1723222b2023ec241f25e3a6b1ad3b97
Mário Russo <mario.russo93@hotmail.com>
2022-11-14 19:46:31 -0300

    Test (service save usuario ) Refatorando o teste

--- agenda/backend/src/test/java/br/com/mariorusso/agenda/core/useCase/UsuarioSaveServiceTest.java
+++ agenda/backend/src/test/java/br/com/mariorusso/agenda/core/useCase/UsuarioSaveServiceTest.java
@@ -2,41 +2,45 @@ package br.com.mariorusso.agenda.core.useCase;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
+
 
 import br.com.mariorusso.agenda.core.model.TipoUsuario;
 import br.com.mariorusso.agenda.core.model.Usuario;
 import br.com.mariorusso.agenda.core.repository.CreateUsuario;
 
-@SpringBootTest()
+@ExtendWith(MockitoExtension.class)
 public class UsuarioSaveServiceTest {
     @InjectMocks
     private UsuarioSaveService saveService;
+    private Usuario usuario;
 
     @Mock
     private CreateUsuario createUsuario;
 
     @Test
     void testSalvar() {
-        Usuario usuario = new Usuario("mario", "mario@russo", "1234", TipoUsuario.ROLE_CLIENTE);
 
-        when(createUsuario.execute(usuario)).thenReturn(usuario);
+        when(createUsuario.execute(any())).thenReturn(usuario);
 
-        saveService.salvar(usuario);
+        Usuario resposta  = saveService.salvar(usuario);
 
-        // assertEquals("mario", resposta.getNome());
-
-        verify(createUsuario,times(1)).execute(usuario);
+        assertEquals("mario", resposta.getNome());
+        assertEquals("mario@russo", resposta.getEmail());
+       
     }
 
     @BeforeEach
     public void init() {
         MockitoAnnotations.openMocks(this);
+        usuario = new Usuario("mario", "mario@russo", "1234", TipoUsuario.ROLE_CLIENTE);
+        saveService = new UsuarioSaveService(createUsuario);
     }
 }
