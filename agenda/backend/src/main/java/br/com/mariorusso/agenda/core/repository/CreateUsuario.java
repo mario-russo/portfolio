fa4f849e344db630c7ed449991753200007be600
Mario Russo <mario.russo93@hotmail.com>
2022-11-15 00:11:52 -0300

    docs (interface createUsuario) : documentação da interface

--- agenda/backend/src/main/java/br/com/mariorusso/agenda/core/repository/CreateUsuario.java
+++ agenda/backend/src/main/java/br/com/mariorusso/agenda/core/repository/CreateUsuario.java
@@ -3,6 +3,16 @@ package br.com.mariorusso.agenda.core.repository;
 import br.com.mariorusso.agenda.core.model.Usuario;
 import br.com.mariorusso.agenda.core.model.responseDto.UsuarioResponseDto;
 
+/**
+ * inteface para salvar o usuário
+ * 
+ * @author Mário Russo
+ */
 public interface CreateUsuario {
+
+    /**
+     * @param usuario
+     * @return Usuário Dto
+     */
     public UsuarioResponseDto execute(Usuario usuario);
 }
